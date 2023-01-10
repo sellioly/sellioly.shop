41f7b0ddab24ff42a7d6aa20bd2e5fa2c12f04b6
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-10 10:30:24 +0100

    authorize sellioly domain in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.hosts << /sellioly\.com/
+  config.hosts << /.*\.sellioly\.com/
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
