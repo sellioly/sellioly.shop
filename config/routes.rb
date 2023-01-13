96ab1d73aa1a751659535e3c098a27e776615387
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 00:18:44 +0100

    add support reading template

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get '/', action: 'index', controller: 'shop'
-  get '/*path', action: 'index', controller: 'shop'
   get '/files/1/assets/:filename', action: 'file_assets', controller: 'shop'
   post 'api/store/create', action: 'create', controller: 'store'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', action: 'index', controller: 'shop'
+  get '/*path', action: 'index', controller: 'shop'
 end
