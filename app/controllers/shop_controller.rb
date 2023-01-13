13bab86886ebbbad7b6e71e3b5b9426171da47ec
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 00:00:31 +0100

    fix database server

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -5,14 +5,14 @@ class ShopController < ApplicationController
     @domain = request.host
     @store = Store.where(app_domain: @domain).first()
     unless @store
-      render json: { msg: 'Store not found', domain:  @domain, store: @store }
+      render json: { msg: 'Store not found', domain: @domain, store: @store }
       return
     end
 
     @path = Rails.root.to_s + @store.template_path
     Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
     @template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
-    @test = @template.render({})
+    @test = @template.render({ page_title: @domain })
     render html: @test.html_safe
   end
 end
