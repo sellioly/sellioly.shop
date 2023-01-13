79f008ae69f4005900a8c47cc50cb4b6441718c9
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-13 22:08:40 +0100

    create default template add job copy template to created store

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/*', action: 'index', controller: 'shop'
+  get '/:any', action: 'index', controller: 'shop', constraint: { any: /.*/ }
   post 'api/store/create', action: 'create', controller: 'store'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
