ee41006c3bcb14762ff0a59cb005b9a91519c975
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 00:36:19 +0100

    add support reading template

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -10,10 +10,9 @@ class ShopController < ApplicationController
     @path = Rails.root.to_s + @store.template_path
     Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
     @template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
-    @test = @template.render('page_title' => @domain )
+    @test = @template.render('page_title' => @domain)
     render html: @test.html_safe
   end
-
 
   def file_assets()
     @domain = request.host
@@ -23,7 +22,6 @@ class ShopController < ApplicationController
       return
     end
     @path = Rails.root.to_s + @store.template_path
-    @content  = File.read(@path + '/assets/' + params[:filename] + '.' + params[:format])
-    render text: @content.to_s
+    render file: @path + '/assets/' + params[:filename] + '.' + params[:format]
   end
 end
