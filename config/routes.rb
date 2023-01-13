96617c04d34c042598c3f42cb89516077f598f3f
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 00:29:28 +0100

    add support reading template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/files/1/assets/:filename', action: 'file_assets', controller: 'shop', constraints: { filename: /.*$/ }
+  get '/files/1/assets/:filename', action: 'file_assets', controller: 'shop', constraint: { filename: /.*$/ }
   post 'api/store/create', action: 'create', controller: 'store'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
