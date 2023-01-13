f176511d93879ae6b1fae6dcac59a02db0669ca3
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-13 22:23:25 +0100

    create default template add job copy template to created store

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -4,17 +4,17 @@ class ShopController < ApplicationController
   def index
     @domain = request.domain
     render json: { msg: @domain }
-    exit(200)
-    @store = Store.where(app_domain: @domain).first()
-    unless @store
-      render json: { msg: 'Store not found' }
-      exit(404)
-    end
-
-    @path = Rails.root.to_s + ("/storage/template")
-    Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
-    @template = Liquid::Template.parse(File.read(@path + '/test.liquid')) # Parses and compiles the template
-    # @items = @template.root.nodelist
-    @test = @template.render('product' => { 'collection' => { 'name' => 'Computer' } }) # => "hi tobi"
+    # exit(200)
+    # @store = Store.where(app_domain: @domain).first()
+    # unless @store
+    #   render json: { msg: 'Store not found' }
+    #   exit(404)
+    # end
+    #
+    # @path = Rails.root.to_s + ("/storage/template")
+    # Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
+    # @template = Liquid::Template.parse(File.read(@path + '/test.liquid')) # Parses and compiles the template
+    # # @items = @template.root.nodelist
+    # @test = @template.render('product' => { 'collection' => { 'name' => 'Computer' } }) # => "hi tobi"
   end
 end
