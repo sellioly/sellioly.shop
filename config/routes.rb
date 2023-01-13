995e755e35947fd19d49e19c77ba62ac72270a7c
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 00:09:26 +0100

    fix database server

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', action: 'index', controller: 'shop'
-  match '/*path', action: 'index', controller: 'shop'
+  get '/*path', action: 'index', controller: 'shop'
   post 'api/store/create', action: 'create', controller: 'store'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
