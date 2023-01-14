e3c20fc7e00376ec8fdc2f68b9cf492a38e8e686
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 15:33:23 +0100

    change default template sellioly

--- app/jobs/upload_local_template_job.rb
+++ app/jobs/upload_local_template_job.rb
@@ -1,6 +1,8 @@
+require 'httparty'
+
 class UploadLocalTemplateJob < ApplicationJob
   queue_as :default
-
+  include HTTParty
   def perform(shop_path, app_domain)
     # Do something later
     unless File.directory?(shop_path)
@@ -10,8 +12,6 @@ class UploadLocalTemplateJob < ApplicationJob
     @path = Rails.root.to_s + ("/storage/template/default")
     FileUtils.copy_entry @path, shop_path
 
-    response = HTTParty.post("https://api.sellioly.com/server/template-created",
-                             :body => { :app_domain => app_domain, :template_path => shop_path }.to_json,
-                             :headers => { 'Content-Type' => 'application/json' })
+    response = HTTParty.post("https://api.sellioly.com/server/template-created", :body => { :app_domain => app_domain, :template_path => shop_path }.to_json, :headers => { 'Content-Type' => 'application/json' })
   end
 end
