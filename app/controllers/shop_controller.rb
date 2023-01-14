b6553cf08a3e7ece1fa1a3b35cd8740889e25453
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-14 09:59:06 +0100

    request paramter not reading

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,5 +1,6 @@
 class ShopController < ApplicationController
   protect_from_forgery except: :file_assets
+
   def index
     @domain = request.host
     @store = Store.where(app_domain: @domain).first
@@ -11,7 +12,8 @@ class ShopController < ApplicationController
     @path = Rails.root.to_s + @store.template_path
     Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
     @template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
-    @test = @template.render('page_title' => @domain, 'request' => { origin: request.base_url })
+    @origin = request.base_url
+    @test = @template.render('request' => { 'origin' => @origin }, 'page_title' => @domain)
     render html: @test.html_safe
   end
 
