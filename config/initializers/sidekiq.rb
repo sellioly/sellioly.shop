2aa6b9c15147a71f660dad24df76bebf3b73cd89
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-15 13:51:33 +0100

    ERR: test deploy

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+# inside config/initializers/sidekiq.rb
+
+Sidekiq.configure_server do |config|
+  config.redis = { url:  ENV['REDIS_CABLE_URL'] }
+end
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_CABLE_URL']}
+end
