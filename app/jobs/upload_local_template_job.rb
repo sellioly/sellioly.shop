bc36005c467c33d96cb59709227e4efdec640ba6
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-15 14:54:41 +0100

    ERR: test deploy

--- app/jobs/upload_local_template_job.rb
+++ app/jobs/upload_local_template_job.rb
@@ -3,7 +3,7 @@ require "http"
 class UploadLocalTemplateJob < ApplicationJob
   queue_as :default
   def perform(shop_path, app_domain)
-    sleep 999
+    sleep 2.minutes
     # Do something later
     unless File.directory?(shop_path)
       FileUtils.mkdir_p(shop_path)
