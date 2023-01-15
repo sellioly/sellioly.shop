e48dfdaf0ed1cb576729a7cff033d223d8e5abf2
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-15 15:38:01 +0100

    fix path uploading

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -15,7 +15,7 @@ class ShopController < ApplicationController
       return
     end
 
-    data  = @response.body.parse
+    @data  = @response.body.parse
 
     @path = Rails.root.to_s + @store.template_path
     Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
@@ -25,7 +25,7 @@ class ShopController < ApplicationController
     @content_for_layout += @test
     @template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
     @origin = request.base_url
-    @test = @template.render('content_for_layout' => @content_for_layout, 'request' => { 'origin' => @origin }, 'page_title' => data['shop_name'])
+    @test = @template.render('content_for_layout' => @content_for_layout, 'request' => { 'origin' => @origin }, 'page_title' => @data['shop_name'])
     render html: @test.html_safe
   end
 
