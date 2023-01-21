dd644148192a22d4ca5d4349eaaf7e6fb50db972
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-21 10:37:29 +0100

    fix docker compose using bind mount

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
-  # constraints host: 'shop.sellioly.com' do
-  #   post 'api/store/create', action: 'create', controller: 'store'
-  #   get '/', action: 'not_found', controller: 'shop'
-  #   get '/*path', action: 'not_found', controller: 'shop'
-  # end
+  constraints host: 'shop.sellioly.com' do
+    post 'api/store/create', action: 'create', controller: 'store'
+    get '/', action: 'not_found', controller: 'shop'
+    get '/*path', action: 'not_found', controller: 'shop'
+  end
 
+  constraints host: 'preview.sellioly.com' do
     get '/screenshots/:shop_id/:template_id', action: 'preview', controller: 'store'
     get '/preview/:shop_id/:template_id', action: 'preview', controller: 'shop'
-    get '/preview', action: 'page', controller: 'shop'
+  end
 
-  # constraints lambda { |req| req.host != 'shop.sellioly.com' &&  req.host != 'preview.sellioly.com' } do
-  #   get '/collections/:collection', action: 'collection', controller: 'shop'
-  #   get '/products/:product', action: 'product', controller: 'shop'
-  #   get '/files/1/assets/:filename', to: 'shop#file_assets', constraints: { filename: /[^\/]+/ }
-  #   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #
-  #   # Defines the root path route ("/")
-  #   # root "articles#index"
-  #   get '/', action: 'index', controller: 'shop'
-  #   get '/*path', action: 'page', controller: 'shop'
-  # end
+  constraints lambda { |req| req.host != 'shop.sellioly.com' &&  req.host != 'preview.sellioly.com' } do
+    get '/collections/:collection', action: 'collection', controller: 'shop'
+    get '/products/:product', action: 'product', controller: 'shop'
+    get '/files/1/assets/:filename', to: 'shop#file_assets', constraints: { filename: /[^\/]+/ }
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    # Defines the root path route ("/")
+    # root "articles#index"
+    get '/', action: 'index', controller: 'shop'
+    get '/*path', action: 'page', controller: 'shop'
+  end
 
   # end
 end
