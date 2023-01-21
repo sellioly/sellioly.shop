dc71218174daee16985267634cd071e4d9a63624
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-21 13:43:06 +0100

    response screenshot as image

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -27,6 +27,6 @@ class StoreController < ApplicationController
     @resize_width = params[:rw].to_s
     @resize_height = params[:rh].to_s
     @response = HTTP.get("https://node-api.sellioly.com/take-screenshot?url=https://preview.sellioly.com/" + params[:shop_id].to_s + "/" + params[:template_id].to_s + '&w=' + @width + '&h=' + @height + '&rw=' + @resize_width + '&rh=' + @resize_height)
-    render status: 200, text:  @response.body
+    send_data @response.body, :filename => 'screenshot.jpeg', :type => 'image/jpeg'
   end
 end
