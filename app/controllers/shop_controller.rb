f64ee140e7f3d90bb5ea3c38074e3b7c4918faaa
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-21 12:12:31 +0100

    check preview function

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -74,7 +74,7 @@ class ShopController < ApplicationController
 
   public def preview
     @path = Rails.root.to_s + '/stroage/' + params[:shop_id] + '/' + params[:template_id]
-    render text: @path
+    render :text => @path
     return
     unless File.directory?(@path)
       content_not_found
