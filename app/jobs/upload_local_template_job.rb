b706690fe0de1a6af8636578ce9bf8e4bdc50b9f
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-21 18:09:41 +0100

    change job upload local template

--- app/jobs/upload_local_template_job.rb
+++ app/jobs/upload_local_template_job.rb
@@ -13,9 +13,13 @@ class UploadLocalTemplateJob < ApplicationJob
     FileUtils.copy_entry @path, shop_path
 
     file = File.read(shop_path + '/config/settings_schema.json')
-    data = JSON.load file
+    @data = JSON.load file
+    @theme_name = @data['theme_name'].to_s
+    @theme_version = @data['theme_version'].to_s
+    @theme_author = @data['theme_author'].to_s
+    @theme_support_url = @data['theme_support_url'].to_s
 
 
-    @response = HTTP.post("https://api.sellioly.com/server/template-created", :form => { 'app_domain' => app_domain, 'template_path' => sub_path, 'theme_name' => data['theme_name'].to_s, 'theme_version' => data['theme_version'].to_s, 'theme_author' => data['theme_author'].to_s, 'theme_support_url' => data['theme_support_url'].to_s })
+    @response = HTTP.post("https://api.sellioly.com/server/template-created", :form => { 'app_domain' => app_domain, 'template_path' => sub_path, 'theme_name' => @theme_name, 'theme_version' => @theme_version, 'theme_author' => @theme_author, 'theme_support_url' => @theme_support_url })
   end
 end
