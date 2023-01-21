ec1d37af95508ab060f51d82eb71a6b58d38c263
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-21 12:20:05 +0100

    check preview function

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -74,12 +74,13 @@ class ShopController < ApplicationController
 
   public def preview
     @path = Rails.root.to_s + '/stroage/' + params[:shop_id] + '/' + params[:template_id]
-    render json: { msg: @path }
-    return
     unless File.directory?(@path)
       content_not_found
       return
     end
+
+    render json: { msg: @path }
+    return
 
     Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
 
