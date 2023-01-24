f2de59c7a17a05359244f9eea8aa038051d072d6
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-01-24 22:03:56 +0100

    change template from second segment to first segment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   constraints host: 'shop.sellioly.com' do
     post 'api/store/create', action: 'create', controller: 'store'
-    post 'api/import/template', action: 'import_template', controller: 'store'
-    post 'api/publish/template', action: 'publish_template', controller: 'store'
+    post 'api/template/import', action: 'import_template', controller: 'store'
+    post 'api/template/publish', action: 'publish_template', controller: 'store'
     get '/', action: 'not_found', controller: 'shop'
     get '/*path', action: 'not_found', controller: 'shop'
   end
