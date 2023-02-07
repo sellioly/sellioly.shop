8fea161099a7dd96c477fc8315e10834a79fc967
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-02-07 15:23:48 +0100

    change server type

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = true
   config.assets.debug = false
+  config.force_ssl = true
 
   # Do not eager load code on boot.
   config.eager_load = true
