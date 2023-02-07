47af332de4e66ef0211d9ad43de517151e54c26b
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-02-07 15:17:50 +0100

    change server type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def check_store
     @domain = request.host
-    @cname = Resolv::DNS.new.getresource(@domain, Types.CNAME)
+    @cname = Resolv::DNS.new.getresource(@domain, Resolv::Types.CNAME)
 
     @store = Store.where(app_domain: @domain).first
     if @store
