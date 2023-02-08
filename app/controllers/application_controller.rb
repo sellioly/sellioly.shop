1120871cf7cd2a1c62aac0d2bb690877e7a5e189
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-02-08 17:16:22 +0100

    change server type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def content_not_found
-    # render file: "#{Rails.root}/public/404.html", layout: true, status: :not_found
-    render  json: {domain: "not found! #{@domain}  #{@cname.inspect}"}
+    render file: "#{Rails.root}/public/404.html", layout: true, status: :not_found
+    # render  json: {domain: "not found! #{@domain}  #{@cname.inspect}"}
   end
 
   def internal_server_error
