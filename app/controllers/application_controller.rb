20664d9193c863f677eca4a047231752198969ef
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-02-25 23:45:31 +0100

    verify domain if allowed or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,13 @@ class ApplicationController < ActionController::Base
     @cname = Resolv::DNS.new.getresource(@domain, Resolv::DNS::Resource::IN::CNAME) rescue nil
     if @cname
       @domain = @cname.name.to_s
+      @cname = @cname.name.to_s
+    end
+
+    @response = HTTP.post("https://api.sellioly.com/server/domain/verify", :form => { 'domain' => @domain, 'app_domain' => @cname })
+    unless @response.status.success?
+      content_not_found
+      return
     end
 
     @store = Store.where(app_domain: @domain).first
