42a553ddbdb8c678f3f1220ce9da9aa0a450a2b0
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-02-26 00:28:22 +0100

    verify domain if allowed or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,12 @@ class ApplicationController < ActionController::Base
       @response = HTTP.post("https://api.sellioly.com/server/domain/verify", :form => { 'domain' => @domain, 'app_domain' => @cname })
       unless @response.status.success?
         content_not_found
-        abort('')
+        Kernel::exit()
       end
       @domain = @cname
     elsif not (@domain =~ /^[A-za-z0-9-.]+.sellioly.com$/)
       content_not_found
-      abort('')
+      Kernel::exit()
     end
 
     @store = Store.where(app_domain: @domain).first
