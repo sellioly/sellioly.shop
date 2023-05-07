5bb3cc5df5945eeb04399308828dabf353525f87
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-07 14:27:55 +0100

    add cart model

--- db/migrate/20230507092334_create_cart_table.rb
+++ db/migrate/20230507092334_create_cart_table.rb
@@ -1,6 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
-    create_table :carts do |t|
+    create_table :cart do |t|
+      t.string :cart_id
+      t.text :items
 
       t.timestamps
     end
