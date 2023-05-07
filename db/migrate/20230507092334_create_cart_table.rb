2e1f06e2861eadef0ce85892e479a10a564ac50c
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-07 18:11:22 +0100

    re-run cart migration

--- db/migrate/20230507092334_create_cart_table.rb
+++ db/migrate/20230507092334_create_cart_table.rb
@@ -2,7 +2,7 @@ class CreateCartTable < ActiveRecord::Migration[7.0]
   def change
     create_table :cart do |t|
       t.string :cart_id
-      t.text :items
+      t.json :items
 
       t.timestamps
     end
