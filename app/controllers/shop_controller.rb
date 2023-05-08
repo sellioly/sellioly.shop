cb4b43873239b0a0a4d5a81ee849f05a3e9c982f
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-08 23:14:52 +0100

    add initialize shop fun (global for all methods)

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,6 +1,8 @@
 class ShopController < ApplicationController
   protect_from_forgery except: :file_assets
 
+  before_action :initialize_shop
+
   def index
     unless check_store
       return
@@ -200,65 +202,15 @@ class ShopController < ApplicationController
   end
 
   public def product
-    unless check_store
-      return
-    end
-
-    unless @store
-      content_not_found
-      return
-    end
-
-    @path = Rails.root.to_s + @store.template_path.to_s
-    Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
-
-    @response = HTTP.post("https://api.sellioly.com/server/store/infos", :form => { 'app_domain' => @domain })
-    unless (@response.status.success?)
-      internal_server_error
-      return
-    end
-    @content_for_layout = ''
-
     file = File.read(@path + '/templates/product.json')
     data = JSON.load file
 
-    @data = @response.parse
-    @shop_name = @data['shop_name']
-    @currency = @data['currency']
-    @shop_description = @data['shop_description']
-    @logo = (@data['shop_logo_default'])
-    args = {}
-    args['logo'] = @logo
-    args['shop_name'] = @shop_name
-
-    @response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => 'main-menu', 'user_id' => $shop_id, 'app_domain' => @domain })
-    args['menu'] = nil
-    if @response.status.success?
-      args['menu'] = @response.parse
+    response = HTTP.post("https://api.sellioly.com/server/product/get-by-handle", :form => { 'handle' => params[:product], 'user_id' => $shop_id, 'app_domain' => @domain })
+    @args['product'] = nil
+    if response.status.success?
+      @args['product'] = response.parse
     end
 
-    # get cart -------------------------------------
-    if cookies[:cart_id].present?
-      cart = Cart.find_by(cart_id: cookies[:cart_id])
-      if cart 
-        args['cart'] = cart
-      else
-        new_cart = Cart.new({})
-        new_cart.cart_id = cookies[:cart_id]
-        new_cart.items = []
-        new_cart.save
-        args['cart'] = new_cart
-      end
-    end
-
-    @response = HTTP.post("https://api.sellioly.com/server/product/get-by-handle", :form => { 'handle' => params[:product], 'user_id' => $shop_id, 'app_domain' => @domain })
-    args['product'] = nil
-    if @response.status.success?
-      args['product'] = @response.parse
-    end
-    args['currency'] = @currency
-    args['page_title'] = "HOME - #{@shop_name}"
-    args['shop_description'] = @shop_description
     if data["order"].kind_of?(Array)
       data["order"].each { |section_id|
         section_data = data["sections"][section_id]
@@ -269,24 +221,23 @@ class ShopController < ApplicationController
             section_blocks.push(section_data['blocks'][block_id])
           }
         end
-        args['section'] = { 'settings' => section_settings, 'blocks' => section_blocks }
+        @args['section'] = { 'settings' => section_settings, 'blocks' => section_blocks }
         unless File.file? @path + '/sections/' + section_data['type'] + '.liquid'
           render plain: 'could not found sections/' + section_data['type'] + '.liquid file missing!', status: 400
           return
         end
-        @template = Liquid::Template.parse(File.read(@path + '/sections/' + section_data['type'] + '.liquid'))
-        @test = @template.render(args)
-        @content_for_layout += @test
+        template = Liquid::Template.parse(File.read(@path + '/sections/' + section_data['type'] + '.liquid'))
+        @content_for_layout += template.render(args)
       }
     end
-    args.delete('section')
+    @args.delete('section')
 
-    @template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
-    @origin = request.base_url
-    args['content_for_layout'] = @content_for_layout
-    args['request'] = { 'origin' => @origin }
-    @test = @template.render(args)
-    render html: @test.html_safe
+    template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
+    origin = request.base_url
+    @args['content_for_layout'] = @content_for_layout
+    @args['request'] = { 'origin' => origin }
+    temp = template.render(args)
+    render html: temp.html_safe
     return
   end
 
@@ -376,9 +327,6 @@ class ShopController < ApplicationController
     return
   end
 
-
-
-
   # def get_global_store_args
   #   @response = HTTP.post("https://api.sellioly.com/server/store/infos", :form => { 'app_domain' => @domain })
   #   unless (@response.status.success?)
@@ -407,4 +355,61 @@ class ShopController < ApplicationController
   #
   #   return args
   # end
+
+  private
+
+  def initialize_shop
+    unless check_store
+      return
+    end
+
+    unless @store
+      content_not_found
+      return
+    end
+
+    @path = Rails.root.to_s + @store.template_path.to_s
+    Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
+
+    response = HTTP.post("https://api.sellioly.com/server/store/infos", :form => { 'app_domain' => @domain })
+    unless (response.status.success?)
+      internal_server_error
+      return
+    end
+    @content_for_layout = ''
+
+    data = response.parse
+    shop_name = data['shop_name']
+    shop_description = data['shop_description']
+    currency = data['currency']
+    logo = (data['shop_logo_default'])
+    
+    @args = {}
+    @args['page_title'] = "HOME - #{shop_name}"
+    @args['shop_name'] = shop_name
+    @args['shop_description'] = shop_description
+    @args['currency'] = currency
+    @args['logo'] = logo
+
+    response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => 'main-menu', 'user_id' => $shop_id, 'app_domain' => @domain })
+    @args['menu'] = nil
+    if response.status.success?
+      @args['menu'] = response.parse
+    end
+
+    # get cart -------------------------------------
+    if cookies[:cart_id].present?
+      cart = Cart.find_by(cart_id: cookies[:cart_id])
+      if cart 
+        args['cart'] = cart
+      else
+        new_cart = Cart.new({})
+        new_cart.cart_id = cookies[:cart_id]
+        new_cart.items = []
+        new_cart.save
+        @args['cart'] = new_cart
+      end
+    end
+  end
+
 end
