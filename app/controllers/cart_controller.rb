71531964be5fa92568bb702fdae6a0c531001350
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-10 12:36:31 +0100

    fix add to cart route

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,60 +1,40 @@
 class CartController < ApplicationController
   public def add
-    if !params[:cart_id].present? || !params[:quantity].present?
+    if !params[:variant_id].present? || !params[:quantity].present? || !cookies[:cart_id].present?
       render json: { error: 'Bad Request' }, status: :bad_request
       return 
     end
 
-    cart_id = params[:cart_id]
+    cart_id = cookies[:cart_id]
+    variant_id = params[:variant_id]
     quantity = params[:quantity]
 
     cart = Cart.find_by(cart_id: cart_id)
+    if !cart
+      render json: { error: 'Cart not found' }, status: :bad_request
+    end
+
     if cart
       updatedItems = cart.items
-
       updatedItems.push({
-        variant_id: '74026596418503',
+        variant_id: variant_id,
         quantity: quantity,
         title: 'JACKET HOMME',
         price: 300,
         image_url: 'https://sellioly.s3.eu-west-3.amazonaws.com/100159510462595346/products/duPchDee4divN7OWaN34GIShN8Q9M3JO9pLJsFAM.jpg',
-        options: {
-          option1: {
+        options: [
+          {
             name: "color",
             value: "black"
           },
-          option2: {
+          {
             name: "size",
             value: "M"
           }
-        }
+        ]
       })
       
       cart.items = updatedItems
-      cart.save
-    else
-      cart = Cart.new({})
-      cart.cart_id = cart_id
-      cart.items = [
-        {
-          variant_id: '74026596418503',
-          quantity: params[:quantity],
-          title: 'JACKET HOMME',
-          price: 300,
-          image_url: 'https://sellioly.s3.eu-west-3.amazonaws.com/100159510462595346/products/duPchDee4divN7OWaN34GIShN8Q9M3JO9pLJsFAM.jpg',
-          options: {
-            option1: {
-              name: "color",
-              value: "black"
-            },
-            option2: {
-              name: "size",
-              value: "M"
-            }
-          }
-        }
-      ]
-
       cart.save
     end
 
