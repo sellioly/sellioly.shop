6c4e95d1092475d49b932a9af7a100483fc0dbe7
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-13 21:42:54 +0100

    ---

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -34,7 +34,7 @@ class OrderController < ApplicationController
       'city' => params[:city],
       'address' => params[:address],
       'phone' => params[:phone],
-      'order_items' => cart.items.to_json,
+      'order_items' => JSON.parse(cart.items.to_json),
     })
     unless response.status.success?
       render json: { error: 'error on create the order!' }, status: :bad_request
