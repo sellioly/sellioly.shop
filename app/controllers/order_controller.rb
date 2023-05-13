91c46dd32c3efa147a689de2aa39de05546a5646
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-13 23:56:05 +0100

    ---

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -45,6 +45,7 @@ class OrderController < ApplicationController
     @path = Rails.root.to_s + @store.template_path.to_s
     unless File.file? @path + '/sections/orderCompleted.liquid'
       render json: {}
+      return
     end
     
     sections = {}
