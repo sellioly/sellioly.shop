cd7bd02d1bfcf856e69f3656a533c9fee1724681
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-13 20:08:25 +0100

    add order endpoint

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,48 @@
+class CartController < ApplicationController
+
+  public def add
+    unless check_store
+      return
+    end
+
+    unless @store
+      content_not_found
+      return
+    end
+
+    # unless cookies[:cart_id].present?
+    unless params[:cart_id].present?
+      render json: { error: 'cart_id required!' }, status: :bad_request
+      return
+    end
+    if !params[:variant_id].present? || !params[:full_name].present? || !params[:phone].present?
+      render json: { error: 'fields required' }, status: :bad_request
+      return 
+    end
+    
+    cart = Cart.find_by(cart_id: params[:cart_id])
+    unless cart
+      render json: { error: 'Cart not found' }, status: :bad_request
+      return
+    end
+
+    response = HTTP.post("https://api.sellioly.com/server/product/variant/get-by-id", :form => { 
+      'app_domain' => @domain,
+      'variant_id' => params[:variant_id],
+      'full_name' => params[:full_name],
+      'email' => params[:email],
+      'city' => params[:city],
+      'address' => params[:address],
+      'phone' => params[:phone],
+      'order_items' => cart
+    })
+    unless response.status.success?
+      render json: { error: 'error on create the order!' }, status: :bad_request
+      return
+    end
+    result = response.parse
+
+    render json: result
+  end
+
+end
