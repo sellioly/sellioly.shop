d68f6098dc1bdaea42a1acb23d06fc042efac9c6
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-13 17:59:35 +0100

    ---

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -214,88 +214,21 @@ class ShopController < ApplicationController
   end
 
   public def collection
-    unless check_store
-      return
-    end
-
-    unless @store
-      content_not_found
-      return
-    end
-
-    @path = Rails.root.to_s + @store.template_path.to_s
-    Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
-
-    @response = HTTP.post("https://api.sellioly.com/server/store/infos", :form => { 'app_domain' => @domain })
-    unless (@response.status.success?)
-      internal_server_error
-      return
-    end
-
-    @data = @response.parse
-    @shop_name = @data['shop_name']
-    @currency = @data['currency']
-    @shop_description = @data['shop_description']
-    @logo = (@data['shop_logo_default'])
-    args = {}
-    args['logo'] = @logo
-    args['shop_name'] = @shop_name
-    args['currency'] = @currency
-    args['page_title'] = "HOME - #{@shop_name}"
-    args['shop_description'] = @shop_description
-
-    @response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => 'main-menu', 'user_id' => $shop_id, 'app_domain' => @domain })
-    args['menu'] = nil
-    if @response.status.success?
-      args['menu'] = @response.parse
-    end
-
-    @response = HTTP.post("https://api.sellioly.com/server/collection/get-by-handle", :form => { 'handle' => params[:collection], 'user_id' => $shop_id, 'app_domain' => @domain })
-    args['collection'] = nil
-    if @response.status.success?
-      args['collection'] = @response.parse
-      @response = HTTP.post("https://api.sellioly.com/server/product/get-by-collection", :form => { 'handle' => params[:collection], 'user_id' => $shop_id, 'app_domain' => @domain })
-      args['collection']['products'] = nil
-      if @response.status.success?
-        args['collection']['products'] = @response.parse
+    response = HTTP.post("https://api.sellioly.com/server/collection/get-by-handle", :form => { 'handle' => params[:collection], 'user_id' => $shop_id, 'app_domain' => @domain })
+    @args['collection'] = nil
+    if response.status.success?
+      @args['collection'] = response.parse
+      response = HTTP.post("https://api.sellioly.com/server/product/get-by-collection", :form => { 'handle' => params[:collection], 'user_id' => $shop_id, 'app_domain' => @domain })
+      @args['collection']['products'] = nil
+      if response.status.success?
+        @args['collection']['products'] = response.parse
       end
     else
       page_not_found
       return
     end
 
-    @content_for_layout = ''
-
-    file = File.read(@path + '/templates/collection.json')
-    data = JSON.load file
-    if data["order"].kind_of?(Array)
-      data["order"].each { |section_id|
-        section_data = data["sections"][section_id]
-        section_settings = section_data['settings']
-        section_blocks = []
-        if section_data['block_order'].kind_of?(Array)
-          section_data['block_order'].each { |block_id|
-            section_blocks.push(section_data['blocks'][block_id])
-          }
-        end
-        args['section'] = { 'settings' => section_settings, 'blocks' => section_blocks }
-        unless File.file? @path + '/sections/' + section_data['type'] + '.liquid'
-          render plain: 'could not found sections/' + section_data['type'] + '.liquid file missing!', status: 400
-          return
-        end
-        @template = Liquid::Template.parse(File.read(@path + '/sections/' + section_data['type'] + '.liquid'))
-        @test = @template.render(args)
-        @content_for_layout += @test
-      }
-    end
-    args.delete('section')
-
-    @template = Liquid::Template.parse(File.read(@path + '/layout/theme.liquid')) # Parses and compiles the template
-    @origin = request.base_url
-    args['content_for_layout'] = @content_for_layout
-    args['request'] = { 'origin' => @origin }
-    @test = @template.render(args)
-    render html: @test.html_safe
+    render_page('collection.json')
     return
   end
 
