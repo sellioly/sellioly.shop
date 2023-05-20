3085ef0fe45f4b13ea2f1c46705fcdf8d723f980
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-20 16:03:06 +0100

    add section tag

--- performance/shopify/section_tag.rb
+++ performance/shopify/section_tag.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
+module Liquid
+  module Tags
+    class SectionTag < Liquid::Tag
+      def initialize(tag_name, markup, options)
+        super
 
-class SectionTag < Liquid::Tag
-  def initialize(tag_name, markup, options)
-    super
+        @name = markup.strip.remove("'")
+      end
 
-    @name = markup.strip.remove("'")
+      def render(context)
+        new_context = context.environments.first
+
+        # Remember here we are not passing extension
+        asset = Template.partial.find_by(filename: 'sections/' + @name + ".liquid")
+
+        Liquid::Template.parse(asset.content).render(new_context).html_safe
+      end
+
+    end
   end
-
-  def render(context)
-    new_context = context.environments.first
-
-    # Remember here we are not passing extension
-    asset = Template.partial.find_by(filename: 'sections/' + @name + ".liquid")
-
-    Liquid::Template.parse(asset.content).render(new_context).html_safe
-  end
-
 end
