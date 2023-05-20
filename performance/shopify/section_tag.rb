36165381ca8972f1b2b5a26dc47b2b7ca684b332
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-21 00:53:24 +0100

    fix cart error in home pagefix cart error in home page

--- performance/shopify/section_tag.rb
+++ performance/shopify/section_tag.rb
@@ -14,7 +14,7 @@ class SectionTag < Liquid::Tag
   def render(context)
     new_context = context.environments.first
 
-    if new_context['section']
+    if new_context.key?('section') and new_context['section']
       return "cannot render section inside other section "
     end
     new_context['section'] = context['layout_data'][@name]
