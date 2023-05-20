6026f1bc4fef5350beb07aab090d512787e1466d
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-20 17:28:24 +0100

    read layout form for liquid

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -103,7 +103,7 @@ class StoreController < ApplicationController
     template = Liquid::Template.parse(File.read(@path + "/layout/#{layout}.liquid"))
     items = template.root.nodelist
     items = items.select{ |node| (node.is_a?(Liquid::Variable)) || node.is_a?(SectionTag)}
-            .map{ |var| var.inspect }
+            .map{ |var| var.name }
     layout_forms = []
     after_content_for_layout = false
     items.each do |name|
