199fdf3b3649c5931dd92d2cf3365b2628406ea5
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-21 01:35:22 +0100

    fix cart error in home pagefix cart error in home page

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -236,12 +236,6 @@ class ShopController < ApplicationController
 
     layout_json = File.read(@path + "/layout/#{layout}.json")
     layout_data = JSON.load layout_json
-    @args['layout_data'] = layout_data['sections']
-    response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => 'main-menu', 'user_id' => $shop_id, 'app_domain' => @domain })
-    @args['menu'] = nil
-    if response.status.success?
-      @args['menu'] = response.parse
-    end
 
     if data["order"].kind_of?(Array)
       data["order"].each { |section_id|
@@ -253,12 +247,76 @@ class ShopController < ApplicationController
           section_schema = JSON.load file
         end
 
+
+        unless section_schema
+          section_data['settings'].each do |_data|
+            key = _data[0]
+            value = _data[1]
+            if section_schema['settings'][key]
+              case section_schema['settings'][key]['type']
+              when 'menu'
+                response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  section_data['settings'][key] = response.parse
+                end
+              when 'product-picker'
+                response = HTTP.post("https://api.sellioly.com/server/product/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  section_data['settings'][key] = response.parse
+                end
+              when 'collection-picker'
+                response = HTTP.post("https://api.sellioly.com/server/collection/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  section_data['settings'][key] = response.parse
+                end
+              else
+                next
+              end
+            else
+
+            end
+          end
+
+        end
+
         section_settings = section_data['settings']
 
         section_blocks = []
         if section_data['block_order'].kind_of?(Array)
           section_data['block_order'].each { |block_id|
-            section_blocks.push(section_data['blocks'][block_id])
+            block_data = section_data['blocks'][block_id]
+            unless section_schema
+              block_schema = section_schema['blocks'][block_data['type']]
+              block_data['settings'].each do |_data|
+                key = _data[0]
+                value = _data[1]
+                if block_schema['settings'][key]
+                  case block_schema['settings'][key]['type']
+                  when 'menu'
+                    response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                    if response.status.success?
+                      block_data['settings'][key] = response.parse
+                    end
+                  when 'product-picker'
+                    response = HTTP.post("https://api.sellioly.com/server/product/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                    if response.status.success?
+                      block_data['settings'][key] = response.parse
+                    end
+                  when 'collection-picker'
+                    response = HTTP.post("https://api.sellioly.com/server/collection/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                    if response.status.success?
+                      block_data['settings'][key] = response.parse
+                    end
+                  else
+                    next
+                  end
+                else
+
+                end
+              end
+            end
+
+            section_blocks.push(block_data)
           }
         end
         @args['section']['settings'] = section_settings
@@ -274,15 +332,47 @@ class ShopController < ApplicationController
 
     @args.delete('section')
 
-    # layout_data['sections'].each do |section|
-    #   variable = section[0].gsub(/^[0-9]+/, "").gsub(/[^A-Za-z0-9]+/, "")
-    #   section_data = section[1]
-    #   @args['section'][variable] = { settings: section_data['settings'] }
-    #   if File.file? @path + '/schemas/' + section_data['type'] + '.json'
-    #     file = File.read(@path + '/schemas/' + section_data['type'] + '.json')
-    #     schema_data = JSON.load file
-    #   end
-    # end
+    layout_data['sections'].each do |section|
+      section_id = section[0]
+      section_data = section[1]
+      schema_data = nil
+      if File.file? @path + '/schemas/' + section_data['type'] + '.json'
+        file = File.read(@path + '/schemas/' + section_data['type'] + '.json')
+        schema_data = JSON.load file
+      end
+      unless schema_data
+        section_data['settings'].each do |_data|
+          key = _data[0]
+          value = _data[1]
+          if schema_data['settings'][key]
+            case schema_data['settings'][key]['type']
+            when 'menu'
+              response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+              if response.status.success?
+                layout_data['sections'][section_id]['settings'][key] = response.parse
+              end
+            when 'product-picker'
+              response = HTTP.post("https://api.sellioly.com/server/product/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+              if response.status.success?
+                layout_data['sections'][section_id]['settings'][key] = response.parse
+              end
+            when 'collection-picker'
+              response = HTTP.post("https://api.sellioly.com/server/collection/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+              if response.status.success?
+                layout_data['sections'][section_id]['settings'][key] = response.parse
+              end
+            else
+              next
+            end
+          else
+
+          end
+        end
+
+      end
+    end
+
+    @args['layout_data'] = layout_data['sections']
 
     template = Liquid::Template.parse(File.read(@path + "/layout/#{layout}.liquid")) # Parses and compiles the template
     origin = request.base_url
