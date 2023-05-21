558aa0942363d938b78f7b07e68f98b3593d75b8
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-21 01:47:45 +0100

    fix cart error in home pagefix cart error in home page

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -284,13 +284,14 @@ class ShopController < ApplicationController
         if section_data['block_order'].kind_of?(Array)
           section_data['block_order'].each { |block_id|
             block_data = section_data['blocks'][block_id]
-            if section_schema
-              if block_data && section_schema['blocks'] && section_schema['blocks'][block_data['type']]
+            if section_schema and block_data
+              if section_schema['blocks'] && section_schema['blocks'][block_data['type']]
                 block_schema = section_schema['blocks'][block_data['type']]
                 block_data['settings'].each do |_data|
                   key = _data[0]
                   value = _data[1]
                   if block_schema['settings'][key]
+                    puts "ELEMENT: " +  block_schema['settings'][key]['element']
                     case block_schema['settings'][key]['element']
                     when 'menu'
                       response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
