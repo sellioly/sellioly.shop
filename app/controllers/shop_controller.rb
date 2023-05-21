0931b8e8fee7fa41c3f25c76ddba1917ceffda94
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-21 10:47:08 +0100

    fix cart error in home pagefix cart error in home page

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -342,10 +342,7 @@ class ShopController < ApplicationController
         schema_data = JSON.load file
       end
       if schema_data
-        puts section_data['settings']
-        puts section_data['settings'].keys.inspect
         section_data['settings'].keys.each do |key|
-          puts schema_data['settings'][key]
           value = section_data['settings'][key]
           if schema_data['settings'][key]
             case schema_data['settings'][key]['type']
