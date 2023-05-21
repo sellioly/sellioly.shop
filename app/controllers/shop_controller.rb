15a0b8ca0eab6483350f77bb6a2628e3e6030ac3
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-05-21 01:38:19 +0100

    fix cart error in home pagefix cart error in home page

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -285,7 +285,7 @@ class ShopController < ApplicationController
           section_data['block_order'].each { |block_id|
             block_data = section_data['blocks'][block_id]
             unless section_schema
-              if section_schema['blocks'] && section_schema['blocks'][block_data['type']]
+              if block_data && section_schema['blocks'] && section_schema['blocks'][block_data['type']]
                 block_schema = section_schema['blocks'][block_data['type']]
                 block_data['settings'].each do |_data|
                   key = _data[0]
