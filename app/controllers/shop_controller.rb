331a61b4d96ebf560df64e57ee38d658f0c430fe
noureddine haddad <noureddinehaddad17@gmail.com>
2023-05-27 20:53:05 +0100

    test

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -101,16 +101,17 @@ class ShopController < ApplicationController
   end
 
   public def page
+    unless request.format.html?
+      content_not_found
+      return
+    end
+
+
     unless check_store
       return
     end
 
     unless @store
-      content_not_found
-      return
-    end
-
-    unless request.format.html?
       content_not_found
       return
     end
