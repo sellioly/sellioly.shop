570c73057f4348bbf3732ce66d60dbc17ea069a9
noureddine haddad <noureddinehaddad17@gmail.com>
2023-06-02 23:34:49 +0100

    move presets from render snippet to initializeshop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -122,6 +122,59 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    # read presets ---------------------------------
+    file = File.read(@path + '/config/settings_data.json')
+    settings_data = JSON.load file
+
+    file = File.read(@path + '/config/settings_schema.json')
+    settings_schema = JSON.load file
+
+    presets = {
+    }
+    if settings_data['presets'][settings_data['current']]
+      settings_data['presets'][settings_data['current']].keys.each do |section_id|
+        section_data = settings_data['presets'][settings_data['current']][section_id]
+        schema_data = settings_schema[section_id]
+        if schema_data
+          section_data['settings'].keys.each do |key|
+            value = section_data['settings'][key]
+            if schema_data['settings'][key]
+              presets[section_id] = { settings: {} }
+              case schema_data['settings'][key]['element']
+              when 'menu'
+                response = HTTP.post("https://api.sellioly.com/server/menu/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  presets[section_id]['settings'][key] = response.parse
+                end
+              when 'product-picker'
+                response = HTTP.post("https://api.sellioly.com/server/product/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  presets[section_id]['settings'][key] = response.parse
+                end
+              when 'products-picker'
+                response = HTTP.post("https://api.sellioly.com/server/product/get-by-handles", :form => { 'handles[]' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  presets[section_id]['settings'][key] = response.parse
+                end
+              when 'collection-picker'
+                response = HTTP.post("https://api.sellioly.com/server/collection/get-by-handle", :form => { 'handle' => value, 'user_id' => $shop_id, 'app_domain' => @domain })
+                if response.status.success?
+                  presets[section_id]['settings'][key] = response.parse
+                end
+              else
+                next
+              end
+            else
+
+            end
+          end
+
+        end
+      end
+
+      @args['presets'] = presets
+    end
+
   end
 
   def render_page(filename)
