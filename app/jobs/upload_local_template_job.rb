68501abc74113c2da48ae6e69be81a4d7b23597a
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-06-09 17:37:10 +0100

    change handles to handles[] problem

--- app/jobs/upload_local_template_job.rb
+++ app/jobs/upload_local_template_job.rb
@@ -9,7 +9,7 @@ class UploadLocalTemplateJob < ApplicationJob
       FileUtils.mkdir_p(shop_path)
     end
 
-    @path = Rails.root.to_s + ("/storage/templates/astral")
+    @path = Rails.root.to_s + ("/storage/templates/default")
     FileUtils.copy_entry @path, shop_path
 
     file = File.read(shop_path + '/config/settings_schema.json')
