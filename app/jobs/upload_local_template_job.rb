e7ec2b2e69f4846db8d46c381202b420419a723b
noureddine haddad <noureddinehaddad17@gmail.com>
2023-06-09 15:49:26 +0100

    change default template 'astral'

--- app/jobs/upload_local_template_job.rb
+++ app/jobs/upload_local_template_job.rb
@@ -9,7 +9,7 @@ class UploadLocalTemplateJob < ApplicationJob
       FileUtils.mkdir_p(shop_path)
     end
 
-    @path = Rails.root.to_s + ("/storage/template/default")
+    @path = Rails.root.to_s + ("/storage/template/astral")
     FileUtils.copy_entry @path, shop_path
 
     file = File.read(shop_path + '/config/settings_schema.json')
