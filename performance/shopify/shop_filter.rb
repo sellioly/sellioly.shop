aac97672d2655a8a6bf7fdbff4af4bb9cc687bd3
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-07-12 18:02:24 +0100

    change globalvariable $ by session

--- performance/shopify/shop_filter.rb
+++ performance/shopify/shop_filter.rb
@@ -7,7 +7,7 @@ module ShopFilter
       context = Liquid::Context.current
 
       "/files/1/#{context['shop_id']}/#{context['template_id']}/assets/#{input}"
-    rescue Liquid::Error => e
+    rescue StandardError  => e
       # Display the error message
       "Liquid Error: #{e.message}"
       # Optionally, you can raise the error again to halt the rendering
