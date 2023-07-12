e69f32e44b59129044bffc41ebb82e0f3af87d96
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-07-12 18:34:08 +0100

    change globalvariable $ by session

--- performance/shopify/shop_filter.rb
+++ performance/shopify/shop_filter.rb
@@ -3,7 +3,7 @@ require 'liquid'
 
 module ShopFilter
   def asset_url(input)
-      "/files/1/#{@context["[shop_id]"]}/#{@context["[template_id]"]}/assets/#{input}"
+      "#{@context.inspect}/files/1/#{@context["[shop_id]"]}/#{@context["[template_id]"]}/assets/#{input}"
   end
 
   def global_asset_url(input)
