6171dd7c8a4fbe0dce164e3c082b3aa11a49d2d0
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2023-07-12 21:02:51 +0100

    change global liquid to instance liquid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -299,7 +299,6 @@ class ApplicationController < ActionController::Base
           return
         end
 
-        @path = Rails.root.to_s + @store.template_path.to_s
         Liquid::Template.file_system = Liquid::LocalFileSystem.new(@path, '%s.liquid')
         template = Liquid::Template.parse(File.read(@path + '/sections/' + section_data['type'] + '.liquid'))
         @content_for_layout += template.render(@args)
