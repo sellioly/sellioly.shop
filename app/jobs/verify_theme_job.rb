a8de2bebaf34639fcbe7b2e9aee106cf061327fb
noureddine haddad <noureddinehaddad17@gmail.com>
2023-07-30 17:34:25 +0100

    ---

--- app/jobs/verify_theme_job.rb
+++ app/jobs/verify_theme_job.rb
@@ -95,15 +95,15 @@ class VerifyThemeJob < ApplicationJob
         FileUtils.rm_rf(@path)
 
         @response = HTTP.post("https://api.sellioly.com/server/theme/verification-result",
-                              :body => { 'status' => 'success', 'url_theme' => url_theme, 'data' => { 'theme_name' => @theme_name, 'theme_version' => @theme_version, 'theme_author' => @theme_author, 'theme_support_url' => @theme_support_url } })
+                              :json => { 'status' => 'success', 'url_theme' => url_theme, 'data' => { 'theme_name' => @theme_name, 'theme_version' => @theme_version, 'theme_author' => @theme_author, 'theme_support_url' => @theme_support_url } })
       else
         @response = HTTP.post("https://api.sellioly.com/server/theme/verification-result",
-                              :body => { 'status' => 'failed', 'url_theme' => url_theme, 'reason' => @error })
+                              :json => { 'status' => 'failed', 'url_theme' => url_theme, 'reason' => @error })
       end
     else
       @error = ['Cannot open your zip file!']
       @response = HTTP.post("https://api.sellioly.com/server/theme/verification-result",
-                            :body => { 'status' => 'failed', 'url_theme' => url_theme, 'reason' => @error })
+                            :json => { 'status' => 'failed', 'url_theme' => url_theme, 'reason' => @error })
     end
   end
 end
