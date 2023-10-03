e3267f5f4a68746c73b823c03e203fae19760241
noureddine haddad <noureddinehaddad17@gmail.com>
2023-10-03 13:12:04 +0100

    (---

--- performance/shopify/snippet_tag.rb
+++ performance/shopify/snippet_tag.rb
@@ -17,7 +17,7 @@ class SnippetTag < Liquid::Tag
       puts value
       puts parse_expression(value)
       
-      @attributes[key] = parse_expression(value)
+      # @attributes[key] = parse_expression(value)
     end
     puts @attributes    
 
