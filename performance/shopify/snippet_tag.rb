6159e9bd32d8a6f6a66e1668a7629954f2473eb1
noureddine haddad <noureddinehaddad17@gmail.com>
2023-10-03 16:08:49 +0100

    ---

--- performance/shopify/snippet_tag.rb
+++ performance/shopify/snippet_tag.rb
@@ -5,16 +5,19 @@ class SnippetTag < Liquid::Tag
   def initialize(tag_name, markup, options)
     super
 
-    @name = markup.strip.remove("'").split(',')[0]
-    unless @name =~ /(.+?)(\.[^.]*$|$)/
-      raise "Illegal template name '#{@name}'"
-    end
+    begin
+      @name = markup.strip.remove("'").split(',')[0]
+      unless @name =~ /(.+?)(\.[^.]*$|$)/
+        raise "Illegal template name '#{@name}'"
+      end
 
-    @attributes = {}
-    markup.scan(Liquid::TagAttributes) do |key, value|
-      @attributes[key] = parse_expression(value)
+      @attributes = {}
+      markup.scan(Liquid::TagAttributes) do |key, value|
+        @attributes[key] = parse_expression(value)
+      end
+    rescue => e
+      puts e.message
     end
-
   end
 
   def render(context)
