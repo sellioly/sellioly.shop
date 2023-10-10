ffa0460816590716bddea0fb14a55f92cfc31da4
noureddine haddad <noureddinehaddad17@gmail.com>
2023-10-10 12:01:40 +0100

    ---

--- performance/shopify/t_filter.rb
+++ performance/shopify/t_filter.rb
@@ -5,29 +5,14 @@ module TFilter
   def t(key)
     begin
       locale = 'en'
-      translation_data = load_translation_data(locale)
-      value = key.split('.').reduce(translation_data) do |data, subkey|
-        data.is_a?(Hash) ? data[subkey] : nil
-      end
 
-      puts value
 
-      return value
+      return locale
     rescue => e
       puts e.message
     end
   end
 
   private
-
-  def self.load_translation_data(locale)
-    begin
-      translation_file_path = Liquid::Template.file_system.root.join('locales', "#{locale}.default.json")
-      puts translation_file_path
-      JSON.parse(File.read(translation_file_path))[locale.to_s]
-    rescue => e
-      puts e.message
-    end
-  end
 
 end
