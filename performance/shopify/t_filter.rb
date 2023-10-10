43c49189b43ae1341df41c20906791c1f2e119ce
noureddine haddad <noureddinehaddad17@gmail.com>
2023-10-10 11:40:14 +0100

    ---

--- performance/shopify/t_filter.rb
+++ performance/shopify/t_filter.rb
@@ -3,25 +3,31 @@ require 'liquid'
 
 module TFilter
   def t(key)
-    locale = 'en'
-    translation_data = load_translation_data(locale)
-    value = key.split('.').reduce(translation_data) do |data, subkey|
-      data.is_a?(Hash) ? data[subkey] : nil
+    begin
+      locale = 'en'
+      translation_data = load_translation_data(locale)
+      value = key.split('.').reduce(translation_data) do |data, subkey|
+        data.is_a?(Hash) ? data[subkey] : nil
+      end
+
+      puts value
+
+      return value
+    rescue => e
+      puts e.message
     end
-
-    puts value
-
-    return value
   end
 
   private
 
   def self.load_translation_data(locale)
-    translation_file_path = Liquid::Template.file_system.root.join('locales', "#{locale}.default.json")
-    puts translation_file_path
-    JSON.parse(File.read(translation_file_path))[locale.to_s]
-  rescue StandardError
-    {}
+    begin
+      translation_file_path = Liquid::Template.file_system.root.join('locales', "#{locale}.default.json")
+      puts translation_file_path
+      JSON.parse(File.read(translation_file_path))[locale.to_s]
+    rescue => e
+      puts e.message
+    end
   end
 
 end
