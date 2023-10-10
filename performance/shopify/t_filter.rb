87f0b1eeba29b63b34d6957eef639e299840fb2d
noureddine haddad <noureddinehaddad17@gmail.com>
2023-10-10 11:55:50 +0100

    ---

--- performance/shopify/t_filter.rb
+++ performance/shopify/t_filter.rb
@@ -18,7 +18,6 @@ module TFilter
   private
 
   def self.load_translation_data(locale)
-    begin
     translation_file_path = Liquid::Template.file_system.root.join('locales', "#{locale}.default.json")
     puts translation_file_path
     JSON.parse(File.read(translation_file_path))[locale.to_s]
