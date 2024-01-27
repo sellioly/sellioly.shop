900a5af4a617434382a2fb14d1e24f90ac72e517
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-01-28 00:25:13 +0100

    - add process event endpoint

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,16 +1,21 @@
 require 'redis'
+
 module StoreHelper
   @redis = Redis.new(url: ENV['REDIS_CABLE_URL'])
 
-  def redis_set(app_domain, shop_id, key, value)
-    @redis.set("domain:#{app_domain}-shop:#{shop_id}.#{key}", value)
-  end
+  class << self
+    attr_reader :redis
 
-  def redis_get(app_domain, shop_id, key)
-    @redis.get("domain:#{app_domain}-shop:#{shop_id}.#{key}")
-  end
+    def redis_set(app_domain, shop_id, key, value)
+      redis.set("domain:#{app_domain}-shop:#{shop_id}.#{key}", value)
+    end
 
-  def redis_del(app_domain, shop_id, key)
-    @redis.del("domain:#{app_domain}-shop:#{shop_id}.#{key}")
+    def redis_get(app_domain, shop_id, key)
+      redis.get("domain:#{app_domain}-shop:#{shop_id}.#{key}")
+    end
+
+    def redis_del(app_domain, shop_id, key)
+      redis.del("domain:#{app_domain}-shop:#{shop_id}.#{key}")
+    end
   end
 end
