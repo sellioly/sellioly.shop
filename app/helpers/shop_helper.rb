ec85b7e167290083c8b9bb3ca956983c3e0f3332
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2024-01-28 01:31:18 +0100

    - change endpoint by redis

--- app/helpers/shop_helper.rb
+++ app/helpers/shop_helper.rb
@@ -3,6 +3,11 @@ module ShopHelper
     Redis.new(url: ENV['REDIS_CABLE_URL'])
   end
 
+
+  def redis_set(app_domain, shop_id, key, value)
+    redis.set("domain:#{app_domain}-shop:#{shop_id}.#{key}", value)
+  end
+
   def redis_get(app_domain, shop_id, key)
     redis.get("domain:#{app_domain}-shop:#{shop_id}.#{key}")
   end
