ee66fb922c6f96f38f3654d912f94a8abc640970
noureddine haddad <noureddinehaddad17@gmail.com>
2025-10-19 18:39:30 +0100

    s

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts.clear
+  config.hosts << /^(?:[A-Za-z0-9-]+\.)*sellioly\.com$/
+  config.hosts << "sellioly.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
