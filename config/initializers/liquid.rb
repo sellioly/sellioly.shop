ab032d59ca1c4ccb35e5126a6cdd3591b2dd248b
noureddine haddad <noureddinehaddad17@gmail.com>
2025-10-25 23:01:45 +0100

    s

---
+++ config/initializers/liquid.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Filters
+Liquid::Template.register_filter(JsonFilter)
+Liquid::Template.register_filter(MoneyFilter)
+Liquid::Template.register_filter(WeightFilter)
+Liquid::Template.register_filter(ShopFilter)
+Liquid::Template.register_filter(TagFilter)
+Liquid::Template.register_filter(TFilter)
+
+# Tags — wrap in to_prepare so dev reloads don’t double-register
+Rails.application.config.to_prepare do
+  Liquid::Template.register_tag('section',  ::Liquid::Tags::SectionTag)
+  Liquid::Template.register_tag('render',   ::Liquid::Tags::RenderTag)
+  Liquid::Template.register_tag('snippet',  ::Liquid::Tags::SnippetTag)
+  Liquid::Template.register_tag('cache',    ::Liquid::Tags::CacheTag)
+  Liquid::Template.register_tag('paginate', ::Liquid::Tags::PaginateTag)
+end
