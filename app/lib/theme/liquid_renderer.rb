72b9a8ddf2e28c6ec63836645fe19f5e0b40ff36
noureddine haddad <noureddinehaddad17@gmail.com>
2025-10-26 02:05:49 +0100

    s

--- app/lib/theme/liquid_renderer.rb
+++ app/lib/theme/liquid_renderer.rb
@@ -19,7 +19,7 @@ module Theme
         previous = Liquid::Template.file_system
         begin
           Liquid::Template.file_system = fs
-          compiled_template.render(assigns)
+          compiled_template.render(assigns, registers: registers)
         ensure
           Liquid::Template.file_system = previous
         end
