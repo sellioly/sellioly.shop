1771c2234f78e2e8a8f716c01ccc3ef032cf9315
noureddine haddad <noureddinehaddad17@gmail.com>
2025-10-26 13:09:06 +0100

    s

--- app/services/pages/page_renderer.rb
+++ app/services/pages/page_renderer.rb
@@ -56,7 +56,7 @@ module Pages
 
         # Render the section via the compiled template
         compiled = compiler.compile(theme_store: store, rel_path: section_liquid)
-        content_for_layout << renderer.render(compiled, assigns: section_assigns, theme_store: store, registers: registers)
+        content_for_layout << renderer.safe_render(compiled, assigns: section_assigns, theme_store: store, registers: registers)
       end
 
       # 3) Merge layout_data from layout JSON (optional)
@@ -73,7 +73,7 @@ module Pages
       assigns = assigns.merge('content_for_layout' => content_for_layout)
       compiled_layout = compiler.compile(theme_store: store, rel_path: layout_rel)
 
-      html = renderer.render(compiled_layout, assigns: assigns, theme_store: store, registers: registers)
+      html = renderer.safe_render(compiled_layout, assigns: assigns, theme_store: store, registers: registers)
 
       # 5) Optional headers (weak ETag based on template + layout file digests)
       etag = weak_etag_for(store, template_name: template_name, layout_rel: layout_rel)
