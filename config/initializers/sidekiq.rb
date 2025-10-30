35d2ee407468b8f93ca716b58a98f5d8394d4b1c
noureddine haddad <noureddinehaddad17@gmail.com>
2025-10-30 13:06:24 +0100

    s

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,6 @@
-# inside config/initializers/sidekiq.rb
-redis_url = ENV['REDIS_SIDEKIQ_URL'] || ENV['REDIS_URL'] || ENV['REDIS_CABLE_URL'] || 'redis://redis:6379/1'
+# config/initializers/sidekiq.rb
+redis_url = ENV['REDIS_SIDEKIQ_URL'] || ENV['REDIS_URL'] || ENV['REDIS_CABLE_URL'] || 'redis://redis:6379/0'
+redis_ns  = ENV.fetch('SIDEKIQ_NAMESPACE', 'sellioly-shop')
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
-end
+Sidekiq.configure_server { |config| config.redis = { url: redis_url, namespace: redis_ns } }
+Sidekiq.configure_client { |config| config.redis = { url: redis_url, namespace: redis_ns } }
