e751fdf09b98476fac2450b9af0339f40ec3f357
noureddine haddad <noureddinehaddad17@gmail.com>
2025-11-15 23:46:23 +0100

    s

--- db/migrate/20251115090030_drop_old_tables.rb
+++ db/migrate/20251115090030_drop_old_tables.rb
@@ -1,4 +1,4 @@
-class DropStoreTable < ActiveRecord::Migration[7.0]
+class DropOldTables < ActiveRecord::Migration[7.0]
   def change
     # Only drop if it exists; safe in dev
     drop_table :stores, if_exists: true
